0816bedcbc34139a4554e8d35ffd205982ceeb10
xyrlix <xyrlix@outlook.com>
2026-04-28 22:21:06 +0800

    feat: 完成 task09 函数定义
    - def 定义函数
    - 带参数函数
    - match-case 多操作符
    - 多返回值函数
    - 更新 progress.md 进度

--- phase1/tasks/week3-4/task09_functions.py
+++ phase1/tasks/week3-4/task09_functions.py
@@ -19,26 +19,57 @@
 """
 
 # TODO: 1. 定义函数 greet()，输出 "Hello!"
-# 在此处写代码
+def greet():
+	print("Hello")
 
 
 
 # TODO: 2. 定义函数 greet_name(name)，输出 "Hello, {name}!"
-# 在此处写代码
+def greet_name(name):
+	print(f"Hello, {name}")
 
 
 
 # TODO: 3. 定义函数 calculate(a, b, op="add")，支持加减乘除
 # op="add" 时返回 a+b，op="sub" 时返回 a-b...
-# 在此处写代码
+def calculate(a, b, op='add'):
+	match op:
+		case "add":
+			return a + b
+		case "sub":
+			return a - b
+		case "mul":
+			return a * b
+		case "div":
+			if b == 0:
+				return -1
+			return a / b
+		case "mod":
+			return a % b
+
+		case _:
+			return -1
+
 
 
 
 # TODO: 4. 定义函数 min_max(numbers)，返回列表中的最小值和最大值
 # 返回两个值：(min_val, max_val)
-# 在此处写代码
+def min_max(numbers):
+	numbers.sort();
+	return numbers[0], numbers[-1]
 
 
 
 # TODO: 5. 调用以上所有函数，并打印结果
 # 在此处写代码
+if __name__ == "__main__":
+	greet()
+
+	greet_name('Bob')
+
+
+	print(calculate(3, 5, "add"))
+
+	numbers = [1, 4, 5, 3, 8]
+	print(min_max(numbers))
